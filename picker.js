5f57f0dc3ca6ed8800d8832e8053216cfcd869fd
Peter Walsh <peter.t.walsh@stonybrook.edu>
2021-03-02 16:53:37 -0500

    Update picker.js

--- picker.js
+++ picker.js
@@ -1,8 +1,12 @@
+var valueChange = new valueChanger(10);
+
 class valueChanger {
+
     constructor(currentValue) {
         this.currentValue = currentValue
         this.maxValue = currentValue;
     }
+    
     change(direction, $currentInput) {
         console.log($currentInput);
         var spinValue = parseInt($currentInput.val());
@@ -23,27 +27,46 @@ class valueChanger {
         }
     }
 }
-var valueChange = new valueChanger(10);
 
-$(init)
+
 var htm="";
-function init() {
+
+function statPickerInit() {
     var currentValueLabel=$("<input/>", { "id":"currentValue", "value": 0, 
     "disabled":"disabled" , "size":2});
 
     var container=$("<div/>",{"id":"stats"});
-    $("body").append(currentValueLabel);
+    $("#statPicker").append(currentValueLabel);
 
     console.log(htm);
     [ "Strength","Wisdom", "Charisma", "Loyalty"].forEach((stat)=>makeStats(stat, container));
     console.log(htm);
+    
+    var submitButton=$("<input/>", { "id":"submitButton", "value": "submit stats" , type:"submit"});
+    var newline=$("<br/>")
+    container.append([newline,submitButton]);
+    console.log($("#submitButton"))
+    
     htm+=container[0].outerHTML;
 
-    $("body").append(htm)
+    $("#statPicker").append(htm)
+    $("#submitButton").on("click",submitStats)
     $('#stats input[type="number"]').niceNumber({
         onIncrement: ($currentInput, amount, settings) => change($currentInput, amount, settings, 1), 
         onDecrement: ($currentInput, amount, settings) => change($currentInput, amount, settings, -1), 
     });
+}
+
+function submitStats(){
+    var niceNumber = $(".nice-number input");
+    var stats = {}
+    niceNumber.each((index,item)=>{
+        var jqItem = $(item);
+        var statType = $(item).attr("id");
+        var statValue = $(item).val();
+        stats[statType]=statValue
+    })
+    console.log(stats);
 }
 
 function change($currentInput, amount, settings, direction) {
@@ -57,6 +80,7 @@ function change($currentInput, amount, settings, direction) {
     //     $currentInput.classList.remove('more-than-100');
     // }
 }
+
 function makeStats(item, container) {
 
     var label=$("<div/>",{"html":item});
