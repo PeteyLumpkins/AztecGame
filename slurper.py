b02a615498eca3182c3a495efaf0db26565cc261
Peter Walsh <peter.t.walsh@stonybrook.edu>
2021-03-23 13:17:28 -0400

    Update slurper.py

--- slurper.py
+++ slurper.py
@@ -11,6 +11,39 @@
 #         passage_split = passage.split("\n")
 #         print(len(passage_split))
 
+def divide_passage(passage, max_length):
+
+    split_passage = passage.split("\n")
+
+    passages = []
+    counter = 0
+    new_passage = ""
+
+    for line in split_passage:
+
+        if counter == max_length:
+            passages.append(new_passage)
+            new_passage = ""
+            counter = 0
+
+        new_passage += line + "\n"
+        counter += 1
+
+    passages.append(new_passage)
+
+    return passages
+
+
+def write_passages(filename, passages):
+
+    f1 = open(filename, "w")
+
+    for passage in passages:
+        f1.write(passage + "\n")
+
+
+    pass
+
 if __name__ == "__main__":
 
     input_filename = "Aztec2.twee"
