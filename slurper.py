2142eb45a6e28238a6e2503d3ffcb7b4cf2ce18f
Peter <Walsh9636@gmail.com>
2021-03-30 12:03:00 -0400

    regular expresion passage split

--- slurper.py
+++ slurper.py
@@ -1,60 +1,79 @@
-# import sys
+import sys
+import re
 
-# with open(sys.argv[1]) as x: f = x.read().split("::")
+with open(sys.argv[1]) as x: f = x.read()
+passages = re.split("(::.*)", f)
 
-# max_passage = 20
+passage_names = passages[1::2]
+passage_content = passages[0::2]
 
-# for passage in f:
-#     passage_length = len(passage)
+max_passage = 20
 
-#     if passage_length > max_passage:
-#         passage_split = passage.split("\n")
-#         print(len(passage_split))
+for passage_index, title_content in enumerate(passage_names):
+    passage_title = re.split("::([^{]*)", title_content)
+    passage_text = passage_content[passage_index]
 
-def divide_passage(passage, max_length):
-
-    split_passage = passage.split("\n")
-
-    passages = []
-    counter = 0
-    new_passage = ""
-
-    for line in split_passage:
-
-        if counter == max_length:
-            passages.append(new_passage)
-            new_passage = ""
-            counter = 0
-
-        new_passage += line + "\n"
-        counter += 1
-
-    passages.append(new_passage)
-
-    return passages
+    passage_text_array = passage_text.split("\n")
+    passage_part_1 = passage_text_array[0: len(passage_text_array) // 2]
+    passage_part_2 = passage_text_array[len(passage_text_array) // 2 :]
 
 
-def write_passages(filename, passages):
-
-    f1 = open(filename, "w")
-
-    for passage in passages:
-        f1.write(passage + "\n")
+    passage_part_2_title = "%s %s []" passage_text_array[1]
+    print(title_content)
+    print(passage_part_1)
 
 
-    pass
+    # if passage_length > max_passage:
+    #     passage_split = passage.split("\n")
+        # print(len(passage_split))
 
-if __name__ == "__main__":
 
-    input_filename = "Aztec2.twee"
-    output_filename = "test.txt"
+# def divide_passage(passage, max_length):
 
-    with open(input_filename) as x: f = x.read().split("::")
+#     split_passage = passage.split("\n")
 
-    passage = f[2]
+#     passages = []
+#     counter = 0
+#     new_passage = ""
 
-    passages = divide_passage(passage, 5)
+#     for line in split_passage:
 
-    write_passages(output_filename, passages)
+#         if counter == max_length:
+#             passages.append(new_passage)
+#             new_passage = ""
+#             counter = 0
+
+#         new_passage += line + "\n"
+#         counter += 1
+
+#     passages.append(new_passage)
+
+#     return passages
+
+
+# def write_passages(filename, passages):
+
+#     f1 = open(filename, "w")
+
+#     for passage in passages:
+#         f1.write(passage + "\n")
+
+
+#     pass
+
+# if __name__ == "__main__":
+
+#     input_filename = "Aztec2.twee"
+#     output_filename = "test.txt"
+
+#     with open(input_filename) as x: f = x.read().split("::")
+
+#     passage = f[2]
+
+#     passages = divide_passage(passage, 20)
+
+#     write_passages(output_filename, passages)
+
+
 
 
